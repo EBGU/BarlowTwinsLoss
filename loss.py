610b59ed990a493fb27989b4061d19719154849c
Yinjun Harold JIA <34360222+EBGU@users.noreply.github.com>
2021-12-07 14:28:42 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+
+class BarlowTwinsLoss(torch.nn.Module):
+
+    def __init__(self, device, lambda_param=5e-3):
+        super(BarlowTwinsLoss, self).__init__()
+        self.lambda_param = lambda_param
+        self.device = device
+
+    def forward(self, z_a: torch.Tensor, z_b: torch.Tensor):
+        # normalize repr. along the batch dimension
+        z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
+        z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
+
+        N = z_a.size(0)
+        D = z_a.size(1)
+
+        # cross-correlation matrix
+        c = torch.mm(z_a_norm.T, z_b_norm) / N # DxD
+        # loss
+        c_diff = (c - torch.eye(D,device=self.device)).pow(2) # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        c_diff[~torch.eye(D, dtype=bool)] *= self.lambda_param
+        loss = c_diff.sum()
+
+        return loss
+
+class BarlowTwinsLoss_m2(torch.nn.Module):
+
+    def __init__(self, device, lambda_param=5e-3):
+        super(BarlowTwinsLoss_m2, self).__init__()
+        self.lambda_param = lambda_param
+        self.device = device
+
+    def forward(self, z_a: torch.Tensor, z_b: torch.Tensor):
+        # normalize repr. along the batch dimension
+        z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
+        z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
+
+        N = z_a.size(0)
+        D = z_a.size(1)
+
+        # cross-correlation matrix
+        c = torch.mm(z_a_norm.T, z_b_norm) / N # DxD
+        # loss
+        c_diff = (c - 2 * torch.eye(D,device=self.device)).pow(2) # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        c_diff[~torch.eye(D, dtype=bool)] *= self.lambda_param
+        loss = c_diff.sum()
+
+        return loss
+
+class BarlowTwinsLoss_LLY(torch.nn.Module):
+
+    def __init__(self, device, lambda_param=5e-3):
+        super(BarlowTwinsLoss_LLY, self).__init__()
+        self.lambda_param = lambda_param
+        self.device = device
+
+    def forward(self, z_a: torch.Tensor, z_b: torch.Tensor):
+        # normalize repr. along the batch dimension
+        z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
+        z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
+
+        N = z_a.size(0)
+        D = z_a.size(1)
+
+        # cross-correlation matrix
+        c = torch.mm(z_a_norm.T, z_b_norm)  # DxD
+        down1 = z_a_norm.pow(2).sum(0, keepdim=True).sqrt()
+        down2 = z_b_norm.pow(2).sum(0, keepdim=True).sqrt()
+        down = torch.mm(down1.t(), down2)
+        c = c / down
+        # loss
+        c_diff = (c - torch.eye(D,device=self.device)).pow(2) # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        c_diff[~torch.eye(D, dtype=bool)] *= self.lambda_param
+        loss = c_diff.sum()
+
+        return loss
+
+class BarlowTwinsLoss_LLYm2(torch.nn.Module):
+
+    def __init__(self, device, lambda_param=5e-3):
+        super(BarlowTwinsLoss_LLYm2, self).__init__()
+        self.lambda_param = lambda_param
+        self.device = device
+
+    def forward(self, z_a: torch.Tensor, z_b: torch.Tensor):
+        # normalize repr. along the batch dimension
+        z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
+        z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
+
+        N = z_a.size(0)
+        D = z_a.size(1)
+
+        # cross-correlation matrix
+        c = torch.mm(z_a_norm.T, z_b_norm)  # DxD
+        down1 = z_a_norm.pow(2).sum(0, keepdim=True).sqrt()
+        down2 = z_b_norm.pow(2).sum(0, keepdim=True).sqrt()
+        down = torch.mm(down1.t(), down2)
+        c = c / down
+        # loss
+        c_diff = (c - 2*torch.eye(D,device=self.device)).pow(2) # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        c_diff[~torch.eye(D, dtype=bool)] *= self.lambda_param
+        loss = c_diff.sum()
+
+        return loss
